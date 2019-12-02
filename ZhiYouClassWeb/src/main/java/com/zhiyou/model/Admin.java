a39ced6d870937808093f722e539ad5e94c338ce
zhang19980904 <1014246130@qq.com>
2019-12-02 15:43:39 +0800

    第一次提交

---
+++ ZhiYouClassWeb/src/main/java/com/zhiyou/model/Admin.java
@@ -0,0 +1,15 @@
+package com.zhiyou.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+	private int admin_id;
+	private	String accounts;
+	private	String password;
+	private	String admin_remark;
+}
